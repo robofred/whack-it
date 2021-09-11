2ecd8adbdba2cf50b80c0919fee0e4dc99012529
Fred Nikgohar <frednikgohar@gmail.com>
2021-09-10 23:07:07 -0700

    Add files via upload

--- code.py
+++ code.py
@@ -1,40 +1,136 @@
-# Connect to an "eval()" service over BLE UART.
+####################################################################################
+#######################     RAPTOR - Testing Firmware       ########################
+#######################     Maybrook Partners LLC - 2021    ########################
+#######################     Commercial Software - All Rights Reserved   ############
+#######################     Author: Fred Nikgohar           ########################
+#######################     Contact: frednikgohar@gmail.com    #####################
+####################################################################################
 
-# TODO: Fred you should read this: https://circuitpython.readthedocs.io/projects/ble/en/latest/api.html
+
+
+####################################################################################
+##############################      PYTHON IMPORTS     #############################
+####################################################################################
+
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
+from digitalio import DigitalInOut, Direction, Pull
+
+import board
+import adafruit_dotstar as dotstar
+
+import time
+import random
+from array import array
+
+from raptorBleName import deviceID, deviceName, deviceList, moleSleep
+
+
+####################################################################################
+#############################      DEVICE CONSTANTS     ############################
+####################################################################################
+
+# On-board DotStar for boards including Gemma, Trinket, and ItsyBitsy
+dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5)
+# For Gemma M0, Trinket M0, Metro M0 Express, ItsyBitsy M0 Express, Itsy M4 Express, QT Py M0
+switch = DigitalInOut(board.D2)
+# switch = DigitalInOut(board.D5)  # For Feather M0 Express, Feather M4 Express
+# switch = DigitalInOut(board.D7)  # For Circuit Playground Express
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+
+# LED setup.
+led = DigitalInOut(board.LED)
+# For QT Py M0. QT Py M0 does not have a D13 LED, so you can connect an external LED instead.
+# led = DigitalInOut(board.SCK)
+led.direction = Direction.OUTPUT
+
+
+
 ble = BLERadio()
-ble.name = "Raptor 3"
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
 
-uart_connections = []
-MAX_CONNECTIONS = 1
-TIME_TO_SCAN = 10
 
-def send_message_to_connections(msg):
-    for uart_connection in ble.connections:        
-        uart_service = uart_connection[UARTService]
-        while uart_connection.connected:
-            uart_service.write(msg.encode("utf-8"))
-            print(uart_service.readline().decode("utf-8"))
+####################################################################################
+#############################      SIMPLE GAME CODE     ############################
+####################################################################################
 
-while True: 
-    print(f"Searching for other devices..., you currently are connected to {len(ble.connections)} devices.")
-    for adv in ble.start_scan(ProvideServicesAdvertisement, timeout=TIME_TO_SCAN):
-        if adv in ble.connections:
-            continue
 
-        if UARTService in adv.services:
-            print("Found a new connection, attempting to connect.")
-            try:
-                ble.connect(adv)
-                print("Connected successfully!")
-            except:
-                print("Failed to connect.")
+# 1 - Generate a Random list of devices from 1 to 5, each coresponding to Device ID (universal list for all devices)
+# 2 - Each device to generate a random number coresponding to each item in List above
+# 3 - If List2 is even (mole) odd (no mole)
+# 4 - All devices get a simultaneous start, and all mole/no-mole are constant time, therefore
+# 5 - As each devic loops through List1, check to see mole, record btn push and/or turn off based on constant time
+# 6 - When List1 finished, publish results
 
-    # ble.stop_scan() may or may not need this
+moleList = []                  # to assign odd/even random numbers to each deviceID in dList
+dListNum = len(deviceList)     # how many devices in deviceList?
 
-    send_message_to_connections(msg)
 
+
+
+def LightsOff():
+    dots[0] = (0,0,0)
+
+
+
+def ResetGame():
+    for x in range(dListNum):      # populate array with odd/even numbers for mole/no-mole
+        # print(deviceList[x])
+        moleID = random.randint(1,100)
+        moleList.append(moleID)
+        # print("Assigned: " + str(moleID))
+        x +=1
+    LightsOff()
+    print("**********************************************")
+    print("*************  GAME WAS RESET  ***************")
+    print("**********************************************")
+
+
+
+
+def SimpleGame():
+    ResetGame()
+    for x in range(dListNum):
+        if (str(deviceList[x]) == str(deviceID)):
+            if (moleList[x] % 2 == 0):
+                print("Mole")
+                dots[0] = (255,0,0)
+                time.sleep(moleSleep)
+            else:
+                print("No Mole")
+                dots[0] = (0,255,0)
+                time.sleep(moleSleep)
+        else:
+            print("I'm OFFFFFFF")
+            dots[0] = (0,0,0)
+            time.sleep(moleSleep)
+    print("DONE!")
+
+
+
+
+while True:
+    ble.start_advertising(advertisement)
+    led.value = False
+    print("Waiting to connect")
+    while not ble.connected:
+        pass
+    ble.name = deviceName
+    print("Connected")
+
+    while ble.connected:
+        s = uart.readline().decode("utf-8")
+        if s:
+            SimpleGame()
+            # myUart = s.split(",", 1)
+            # gameBlink = int(myUart[0])
+            # gameTime = float(myUart[1])
+            # PatternGame(gameBlink, gameTime)
+            LightsOff()
+            uart.write("Done")
